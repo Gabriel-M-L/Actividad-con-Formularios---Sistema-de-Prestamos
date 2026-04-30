af20f8662418e82bba676cf0d092f484c6f48b6a
GABRIEL MARTINEZ LOUFER <49960445@ort>
2026-04-30 08:52:56 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Actividad_con_formularios.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
